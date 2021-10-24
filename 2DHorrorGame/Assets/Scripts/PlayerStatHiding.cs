fdd9bfe9ea5bedddc2705523634b62dc34881895
Marcel <cisowski@student.agh.edu.pl>
2021-10-24 11:13:18 +0200

    State Machine Script Update

--- 2DHorrorGame/Assets/Scripts/PlayerStatHiding.cs
+++ 2DHorrorGame/Assets/Scripts/PlayerStatHiding.cs
@@ -2,5 +2,16 @@ using UnityEngine;
 
 public class PlayerStateHiding : PlayerStateBase
 {
+    public override void EnterState(PlayerStateMachine player)
+    {
 
+    }
+    public override void UpdateState(PlayerStateMachine player)
+    {
+
+    }
+    public override void OnCollisionEnter(PlayerStateMachine player)
+    {
+
+    }
 }
