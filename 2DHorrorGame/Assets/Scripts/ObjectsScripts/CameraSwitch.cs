a1bfb86cb4a1f8fad873c502f889bfe2e14391bd
Marcel <cisowski@student.agh.edu.pl>
2021-11-06 11:52:46 +0100

    Camera Switching added

---
+++ 2DHorrorGame/Assets/Scripts/ObjectsScripts/CameraSwitch.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraSwitch : MonoBehaviour
+{
+    [SerializeField] CinemachineVirtualCamera playerCamera1;
+    [SerializeField] CinemachineVirtualCamera playerCamera2;
+    [SerializeField] float middleOfTheTriggger;
+
+    private void Awake()
+    {
+        playerCamera1.Priority = 1;
+        playerCamera2.Priority = 0;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            if (collision.transform.position.x < middleOfTheTriggger)
+            {
+                playerCamera1.Priority = 0;
+                playerCamera2.Priority = 1;
+            }
+            if (collision.transform.position.x > middleOfTheTriggger)
+            {
+                playerCamera1.Priority = 1;
+                playerCamera2.Priority = 0;
+            }
+        }
+    }
+
+}
