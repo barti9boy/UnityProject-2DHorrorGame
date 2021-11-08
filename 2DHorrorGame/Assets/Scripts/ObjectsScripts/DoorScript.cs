46653759461a3809ecdfc412e4670f8d83ce0258
Marcel <cisowski@student.agh.edu.pl>
2021-11-08 13:29:15 +0100

    Door probably working

--- 2DHorrorGame/Assets/Scripts/ObjectsScripts/DoorScript.cs
+++ 2DHorrorGame/Assets/Scripts/ObjectsScripts/DoorScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,11 +7,19 @@ public class DoorScript : MonoBehaviour
 {
     public bool isLocked;
     public bool isOpened;
+    public bool isChangingRoom = false;
+    public int velocityDirection;
 
     public float interactionTime;
     public float unlockTimeRequired;
     [SerializeField] private int itemIdToUnlock;
+    [SerializeField] private Transform rightPoint;
+    [SerializeField] private Transform leftPoint;
+    [SerializeField] private float movementSpeed = 5.0f;
+
     private Collider2D doorCollider;
+    private Rigidbody2D playerRb;
+    private Transform playerTransform;
      
     private void Awake()
     {
@@ -19,7 +28,10 @@ public class DoorScript : MonoBehaviour
     }
     private void Update()
     {
-        
+        if (isChangingRoom)
+        {
+            MovePlayer();
+        }
     }
     public void DoorUnlock(List<int> itemIDs, bool isInteractionButtonHeld)
     {
@@ -44,11 +56,18 @@ public class DoorScript : MonoBehaviour
             }
         }
     }
-    public void DoorOpenOrClose()
+    public void DoorOpen()
     {
-        isOpened = !isOpened;
-        doorCollider.enabled = !doorCollider.enabled;
+        isOpened = true;
+        doorCollider.enabled = false;
     }
+
+    public void DoorClose()
+    {
+        isOpened = false;
+        doorCollider.enabled = true;
+    }
+
 
     public void DoorInteractionTimer(bool isInteractionButtonHeld)
     {
@@ -59,6 +78,57 @@ public class DoorScript : MonoBehaviour
         else
         {
             interactionTime = 0;
+        }
+    }
+
+    public void ChangeRoom(Transform transform ,Rigidbody2D rb, InputManager inputManager, PlayerStateMachine player)
+    {
+        playerRb = rb;
+        playerTransform = transform;
+        if(Math.Abs(rightPoint.transform.position.x - playerTransform.position.x) > Math.Abs(leftPoint.transform.position.x - playerTransform.position.x)) //jesteœmy po lewej
+        {
+            if (!player.currentState.isFacingRight)
+            {
+                playerTransform.Rotate(0, 180, 0);
+                player.currentState.isFacingRight = !player.currentState.isFacingRight;
+                Debug.Log(player.currentState.isFacingRight);
+                inputManager.isInteractionButtonClicked = false;
+            } 
+            velocityDirection = 1;
+        }
+        else if (Math.Abs(rightPoint.transform.position.x - playerTransform.position.x) < Math.Abs(leftPoint.transform.position.x - playerTransform.position.x)) // jesteœmy po prawej
+        {
+            if (player.currentState.isFacingRight)
+            {
+                playerTransform.Rotate(0, 180, 0);
+                player.currentState.isFacingRight = !player.currentState.isFacingRight;
+                Debug.Log(player.currentState.isFacingRight);
+                inputManager.isInteractionButtonClicked = false;
+            }
+            velocityDirection = -1;
+        }
+        isChangingRoom = true;
+    }
+
+    public void MovePlayer()
+    {
+        if(velocityDirection == 1 && playerTransform.position.x < rightPoint.position.x)
+        {
+            playerRb.velocity = new Vector2(velocityDirection * movementSpeed, 0);
+            if(Math.Abs(playerTransform.position.x - rightPoint.transform.position.x) < 0.1)
+            {
+                isChangingRoom = false;
+                DoorClose();
+            }
+        }
+        if (velocityDirection == -1 && playerTransform.position.x > leftPoint.position.x)
+        {
+            playerRb.velocity = new Vector2(velocityDirection * movementSpeed, 0);
+            if (Math.Abs(playerTransform.position.x - leftPoint.transform.position.x) < 0.1)
+            {
+                isChangingRoom = false;
+                DoorClose();
+            }
         }
     }
 }
