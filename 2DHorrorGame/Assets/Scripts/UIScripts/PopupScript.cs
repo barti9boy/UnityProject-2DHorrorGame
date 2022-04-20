594f28486abaf7a32de80d44cff028bf3613df5f
Bartek Jaromin <83396630+barti9boy@users.noreply.github.com>
2022-04-20 15:25:48 +0200

    tutorial popups

--- 2DHorrorGame/Assets/Scripts/UIScripts/PopupScript.cs
+++ 2DHorrorGame/Assets/Scripts/UIScripts/PopupScript.cs
@@ -1,18 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PopupScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public TextMeshProUGUI popupText;
+    [SerializeField] private Animator popupAnimator;
+
+    public bool popupIn, popupShow, popupOut, popupHide;
+    
+    public void Awake()
     {
-        
+        popupHide = true;
+        popupIn = false;
+        popupShow = false;
+        popupOut = false;
+        UpdateAnimations();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PopupOut()
     {
+        popupOut = true;
+        popupIn = false;
+        popupShow = false;
+        popupHide = false;
+        UpdateAnimations();
+    }
+    public void PopupIn()
+    {
+        popupIn = true;
+        popupOut = false;
+        popupHide = false;
+        popupOut = false;
+        UpdateAnimations();
+    }
+    public void PopupShow()
+    {
+        popupShow = true;
+        popupHide = false;
+        popupIn = false;
+        popupOut = false;
+        UpdateAnimations();
         
+    }
+    public void PopupHide()
+    {
+        popupHide = true;
+        popupShow = false;
+        popupOut = false;
+        popupIn = false;
+        UpdateAnimations();
+        PopupSetInactive();
+    }
+    public void PopupSetMessage(string message)
+    {
+        popupText.text = message;
+    }
+    public void PopupSetInactive()
+    {
+        Time.timeScale = 1f;
+        popupText.text = "";
+        this.gameObject.SetActive(false);
+    }
+    public void UpdateAnimations()
+    {
+        popupAnimator.SetBool("PopupHide", popupHide);
+        popupAnimator.SetBool("PopupIn", popupIn);
+        popupAnimator.SetBool("PopupShow", popupShow);
+        popupAnimator.SetBool("PopupOut", popupOut);
     }
 }
---
+++ 2DHorrorGame/Assets/Scripts/UIScripts/PopupTriggerScript.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupTriggerScript : MonoBehaviour
+{
+    
+    public GameObject popup;
+    private PopupScript popupScript;
+    [Header("Input message in CAPS")]
+    [SerializeField] private string message;
+    private void Awake()
+    {
+        popupScript = popup.GetComponent<PopupScript>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Time.timeScale = 0f;
+            popup.SetActive(true);
+            popupScript.PopupSetMessage(message);
+            popupScript.PopupIn();
+            this.gameObject.SetActive(false);
+        }
+    }
+}
