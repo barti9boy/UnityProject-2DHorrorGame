20eb4a58c527d1213549efaee9716d81289c2288
Bartek Jaromin <83396630+barti9boy@users.noreply.github.com>
2022-05-04 14:26:52 +0200

    small tweaks 2

--- 2DHorrorGame/Assets/Scripts/ObjectsScripts/InteractionHighlight.cs
+++ 2DHorrorGame/Assets/Scripts/ObjectsScripts/InteractionHighlight.cs
@@ -7,11 +7,14 @@ public class InteractionHighlight : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Material material;
+
+    private float colorAlpha = 0.15f;
     private void Awake()
     {
         spriteRenderer.sprite = GetComponentInParent<SpriteRenderer>().sprite;
         spriteRenderer.sortingOrder = GetComponentInParent<SpriteRenderer>().sortingOrder + 1;
         spriteRenderer.sortingLayerName = GetComponentInParent<SpriteRenderer>().sortingLayerName;
+        spriteRenderer.color = new Vector4(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, colorAlpha);
         spriteRenderer.material = material;
     }
     private void OnTriggerEnter2D(Collider2D collision)
