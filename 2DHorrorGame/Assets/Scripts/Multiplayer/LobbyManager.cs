a8b29c44ff002165fa990368895febd847687d75
Bartek Jaromin <83396630+barti9boy@users.noreply.github.com>
2022-11-16 18:29:13 +0100

    server setup
    first steps setting up the server

---
+++ 2DHorrorGame/Assets/Scripts/Multiplayer/LobbyManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    public InputField createRoomInput;
+    public InputField joinRoomInput;
+
+    public void CreateRoom()
+    {
+
+    }
+    public void JoinRoom()
+    { 
+        
+    }
+}
