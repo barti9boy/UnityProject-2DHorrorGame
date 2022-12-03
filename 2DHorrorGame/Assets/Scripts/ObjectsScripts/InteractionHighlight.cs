52aead9fc8b37e5e5c058d48457001195a3def37
Bartek Jaromin <83396630+barti9boy@users.noreply.github.com>
2022-12-03 10:43:14 +0100

    new interaction highlight

--- 2DHorrorGame/Assets/Scripts/ObjectsScripts/InteractionHighlight.cs
+++ 2DHorrorGame/Assets/Scripts/ObjectsScripts/InteractionHighlight.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using TMPro;
 
-//NOTE: when adding this prefab you have to set sorting layer and sprite  manually in inspector
+//NOTE: when adding this prefab you have to set sorting layer and sprite manually in inspector
 public class InteractionHighlight : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Material material;
     private float colorAlpha = 0.15f;
+
+    private TextMeshProUGUI interactionText;
+    [SerializeField] private string interactionTextContent;
     private void Awake()
     {
         spriteRenderer.sprite = GetComponentInParent<SpriteRenderer>().sprite;
@@ -18,11 +22,18 @@ public class InteractionHighlight : MonoBehaviour
         spriteRenderer.color = new Vector4(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, colorAlpha);
         spriteRenderer.material = material;
     }
+    private void Start()
+    {
+        interactionText = transform.GetChild(0).GetChild(0).GetComponent<TextMeshProUGUI>();
+        interactionText.gameObject.SetActive(false);
+        interactionText.text = interactionTextContent;
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            spriteRenderer.enabled = true;   
+            spriteRenderer.enabled = true;
+            interactionText.gameObject.SetActive(true);
         }
     }
     private void OnTriggerExit2D(Collider2D collision)
@@ -30,6 +41,7 @@ public class InteractionHighlight : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             spriteRenderer.enabled = false;
+            interactionText.gameObject.SetActive(false);
         }
     }
 }
