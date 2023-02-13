c2bc17db673e3d73364284a1b4de4e04868c05c9
Bartek Jaromin <83396630+barti9boy@users.noreply.github.com>
2023-02-13 23:29:23 +0100

    build and tutorial settings

--- 2DHorrorGame/Assets/Scripts/UIScripts/PopupTriggerScript.cs
+++ 2DHorrorGame/Assets/Scripts/UIScripts/PopupTriggerScript.cs
@@ -12,7 +12,8 @@ public class PopupTriggerScript : MonoBehaviour
     private void Awake()
     {
         popupScript = popup.GetComponent<PopupScript>();
-        message = "DAY 1543, ID:203 \n ";
+        message = "LAB NOTES; DAY 1543 \n \n ALL OF THE SUBJECTS ESCAPED THEIR LABORATORY CELLS AND GOT FREE \n MANY TURNED INTO UNDIFINED CREATURES AND ARE HOSTILE TOWARDS ANY LIVING BEEINGS. \n THEY ARE SENSITIVE TO LIGHT, SO FLASHLIGHT [F] HAS TO BE USE CAUTIOUSLY" +
+            "\n THE POWER IN FLASHLIGTS RUN OUT QUICKLY SO USE IT IS REQUIRED TO USE THEM PRUDENTLY";
 
 
     }
